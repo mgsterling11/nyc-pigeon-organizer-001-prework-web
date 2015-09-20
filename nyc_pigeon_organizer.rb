b59ce25f681c5825befb68d33db77e72d13b88cb
mgsterling11 <m.g.sterling11@gmail.com>
2015-09-19 20:25:49 -0400

    Done -- but had to look up solution; have question about ||=

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+   nyc_pigeons = {}
+
+   data.each do |key, value|
+     value.each do |description, pigeons|
+       pigeons.each do |pigeon|
+         nyc_pigeons[pigeon] ||= {}
+         nyc_pigeons[pigeon][key] ||= []
+         nyc_pigeons[pigeon][key] << description.to_s
+       end
+     end
+   end
+   nyc_pigeons
+ end
+
